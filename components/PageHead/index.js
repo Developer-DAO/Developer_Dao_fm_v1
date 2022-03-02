13357efa1ac1bd99ccf006abfaa28d8bbc644a50
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2022-03-02 14:45:00 +0200

    SEO update

--- components/PageHead/index.js
+++ components/PageHead/index.js
@@ -1,51 +1,59 @@
 import Head from 'next/head'
-import { NextSeo } from 'next-seo';
 
 const PageHead = () =>{
     return(
-      <NextSeo
-      title="Developer DAO FM 🎧"
-      description="A place where you can chill and listen to Lofi music while building cool things!"
-      canonical="https://www.developerdaofm.com/"
-      openGraph={{
-        url: 'https://www.url.ie/a',
-        title: 'Developer DAO FM 🎧',
-        description: 'A place where you can chill and listen to Lofi music while building cool things!',
-        images: [
-          {
-            url: 'https://i.ibb.co/tPLLryx/1.png',
-            width: 800,
-            height: 600,
-            alt: 'Developer DAO FM',
-            type: 'image/jpeg',
-          },
-          {
-            url: 'https://i.ibb.co/wdwWkdz/3669469.png',
-            width: 900,
-            height: 800,
-            alt: 'Developer DAO FM',
-            type: 'image/jpeg',
-          },
-          { url: 'https://www.example.ie/og-image-03.jpg' },
-          { url: 'https://www.example.ie/og-image-04.jpg' },
-        ],
-        site_name: 'Developer DAO FM',
-      }}
-      twitter={{
-        handle: 'developer_dao',
-        site: 'developerdaofm.com',
-        cardType: 'summary_large_image',
-      }}
-    />
+       <Head>
+        <title>Developer DAO FM 🎧</title>
+        <meta name="description" content="A place where you can chill and listen to Lofi music while building cool things!" />
+        <link rel="icon" href="/Frame 2.png" sizes="16x16" />
 
-    //   <NextSeo
-    //   title="Developer DAO FM 🎧"
-    //   description="a place where  you can chill and listen to lofi music"
-    // />
-    // <title></title>
-    // <meta name="description" content="" />
-    // <link rel="icon" href="/Frame 2.png" sizes="16x16" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@developer_dao" />
+        <meta name="twitter:title" content="Developer DAO FM 🎧" />
+        <meta name="twitter:description" content="A place where you can chill and listen to Lofi music while building cool things!" />
+        <meta name="twitter:image" content="https://i.ibb.co/tPLLryx/1.png" />
+
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Developer DAO FM 🎧" />
+        <meta property="og:description" content="A place where you can chill and listen to Lofi music while building cool things!" />
+        <meta property="og:url" content="https://www.developerdaofm.com/" />
+        <meta property="og:image" content="https://i.ibb.co/tPLLryx/1.png" />
+      </Head>
     )
 }
 export default PageHead
 
+{/* <NextSeo
+title="Developer DAO FM 🎧"
+description="A place where you can chill and listen to Lofi music while building cool things!"
+canonical="https://www.developerdaofm.com/"
+openGraph={{
+  url: 'https://www.url.ie/a',
+  title: 'Developer DAO FM 🎧',
+  description: 'A place where you can chill and listen to Lofi music while building cool things!',
+  images: [
+    {
+      url: 'https://i.ibb.co/tPLLryx/1.png',
+      width: 800,
+      height: 600,
+      alt: 'Developer DAO FM',
+      type: 'image/jpeg',
+    },
+    {
+      url: 'https://i.ibb.co/wdwWkdz/3669469.png',
+      width: 900,
+      height: 800,
+      alt: 'Developer DAO FM',
+      type: 'image/jpeg',
+    },
+    { url: 'https://www.example.ie/og-image-03.jpg' },
+    { url: 'https://www.example.ie/og-image-04.jpg' },
+  ],
+  site_name: 'Developer DAO FM',
+}}
+twitter={{
+  handle: 'developer_dao',
+  site: 'developerdaofm.com',
+  cardType: 'summary_large_image',
+}}
+/> */}
