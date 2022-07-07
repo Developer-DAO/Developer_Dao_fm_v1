ba38128915c165d4c32f12f0ef5632a1f3d761bb
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2022-07-07 18:00:46 +0200

    adding rel="noreferrer"

--- components/ProductHunt/index.js
+++ components/ProductHunt/index.js
@@ -4,9 +4,8 @@ const ProductHunt = () =>{
 
         <div id="ph">
             <a href="https://www.producthunt.com/posts/developer-dao-fm?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-developer&#0045;dao&#0045;fm"
-            target="_blank">   
-            <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=351730&theme=dark"
-            alt="Developer&#0032;DAO&#0032;FM - Listen&#0032;to&#0032;Lofi&#0032;music&#0032;while&#0032;building&#0032;cool&#0032;stuff | Product Hunt" 
+            target="_blank" rel="noreferrer" >   
+            <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=351730&theme=dark" alt="Developer&#0032;DAO&#0032;FM - Listen&#0032;to&#0032;Lofi&#0032;music&#0032;while&#0032;building&#0032;cool&#0032;stuff | Product Hunt" 
             style={{"width": "250px", "height": "54px" }}
             width="250" 
             height="54" />
